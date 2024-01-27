151567b70bd1188ec8b701b106f0aff95fb5721e
Jonathan Saenger <jonathan.saenger@outlook.fr>
2024-01-27 20:10:04 +0100

    MAIN > Création d'un tableau de Contact en PHP et vue sur Twig

--- src/Controller/AnnuaireController.php
+++ src/Controller/AnnuaireController.php
@@ -11,8 +11,28 @@ class AnnuaireController extends AbstractController
     #[Route('/', name: 'app_annuaire')]
     public function index(): Response
     {
+
+        $contacts = [
+            [
+                'nom' => 'Dupont',
+                'prenom' => 'Jean',
+                'telephone' => '0123456789',
+            ],
+            [
+                'nom' => 'Durand',
+                'prenom' => 'Marie',
+                'telephone' => '0123456789',
+            ],
+            [
+                'nom' => 'Saenger',
+                'prenom' => 'Jonathan',
+                'telephone' => '0123456789',
+            ],
+        ];
+
+
         return $this->render('annuaire/index.html.twig', [
-            'controller_name' => 'AnnuaireController',
+            'contacts' => $contacts,
         ]);
     }
 }
